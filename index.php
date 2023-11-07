d7fbde1425944126502607a0629da8eb4f75f348
Tayfun <59694128+RIPeaglE@users.noreply.github.com>
2023-11-07 16:47:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,16 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous" defer></script>
     <title>SQL</title>
 </head>
 <body>
-    <h2>Form</h2>
+    
+    <h2>Fill the form</h2>
     <form action="" method="POST" enctype="multipart/form-data">
-        <input type="text" name="product" placeholder="name"> 
-        <input type="number" name="price" placeholder="price"> 
-        <input type="text" name="description" placeholder="description">
-        <input type="file" name="image" accept="image/*" />
+        <input type="text" name="product" placeholder="name" required> 
+        <input type="number" name="price" placeholder="price" required> 
+        <input type="text" name="description" placeholder="description" required>
+        <input type="file" name="image" accept="image/*" required/>
         <input type="submit" name="submit" value="submit">
     </form>
         <div>
@@ -29,15 +31,9 @@
         $description = $_POST['description'];
         $price = $_POST['price'];
         $image = $_FILES['image']['name']; 
-        if (isset($_POST['submit'])){
+        if (isset($_POST['submit'])){   
             include 'funktioner/insert.php';
         }
-    ?> 
-
-  
-
-
-
-    
+    ?>     
 </body>
 </html>
