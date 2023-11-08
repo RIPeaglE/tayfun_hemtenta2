ba0a96a775e6d965b38f97de505ca5059f1fd077
Tayfun <ripeaglee@hotmail.com>
2023-11-08 11:53:20 +0100

    boxes for the form and uptate form

--- index.php
+++ index.php
@@ -8,31 +8,66 @@
     <title>SQL</title>
 </head>
 <body>
-    <div class="p-3 m-0 border-0 bd-example m-0 border-0">
-    <form method="POST" enctype="multipart/form-data" class="row gx-3 gy-2 align-items-center" >
-      <div class="col-auto">
-        <input type="text" class="form-control" name="product" placeholder="Product name">
-      </div>
-      <div class="col-auto">
-        <input type="text" class="form-control" name="description" placeholder="Description">
-      </div>
-      <div class="col-auto">
-        <input type="number" class="form-control" name="price" placeholder="price">
-      </div>
+    <div class="container">
+        <div class="row">
+            <!-- Left Column (Form) -->
+            <div class="col-md-6">
+                <div class="p-3 border border-primary">
+                    <form method="POST" enctype="multipart/form-data" class="row gx-3 gy-2 align-items-center">
+                        <div class="col-auto">
+                            <input type="text" class="form-control" name="product" placeholder="Product name">
+                        </div>
+                        <div class="col-auto">
+                            <input type="text" class="form-control" name="description" placeholder="Description">
+                        </div>
+                        <div class="col-auto">
+                            <input type="number" class="form-control" name="price" placeholder="Price">
+                        </div>
+                        <div class="col-auto">
+                            <input type="file" class="form-control" name="image" accept="image/*">
+                        </div>
+                        <div class="col-auto">
+                            <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
 
-      <div class="col-auto">
-        <input type="file" class="form-control" name="image" accept="image/*">
-      </div>
-      <div class="col-auto">
-        <button type="submit" name="submit" value="submit" class="btn btn-primary">submit</button>
-      </div>
-      <button class="btn btn-primary">
-            Uppdate
-        </button>
-    </form>
-        
+            <!-- Right Column (Content) -->
+            <div class="col-md-6">
+            <form action="" class="d-flex flex-column mb-3" method="POST" enctype="multipart/form-data">
+                    <input type="number" placeholder="Product ID" class="form-control" name="updateID" required>
+                    <input type="number" placeholder="Price" class="form-control" name="newPrice">
+                    <input type="file" placeholder="Image Link" class="form-control" name="newImage">
+                    <button class="btn btn-danger" name="uppButton">
+                        Uppdate information
+                    </button>
+                </form>
+                    <!-- php code to get variable of the form and include the uppdate file-->
+                <?php
+                    $id = $_POST['updateID'];
+                    $newPrice = $_POST['newPrice'];
+                    $newImage = $_FILES['newImage']['name'];
+                        // Check for a post from the button and then include the file
+                    if ( isset($_POST['uppButton'])){
+                        include 'funktioner/uppdate.php';
+                    } 
+                ?>
+            </div>
+        </div>
 
-  </div>
+        <!-- Update Button Row -->
+        <div class="row mt-3">
+            <div class="col-12">
+                <form method="post">
+                    <button class="btn btn-primary w-100">
+                        Update
+                    </button>
+                </form>    
+            </div>
+        </div>
+    </div>
+
     <div>
         <?php
             include 'funktioner/view.php';
@@ -49,7 +84,6 @@
         if (isset($_POST['submit'])){   
             include 'funktioner/insert.php';
         }
-
-    ?>     
-</body>
+    ?>
+<body>
 </html>
